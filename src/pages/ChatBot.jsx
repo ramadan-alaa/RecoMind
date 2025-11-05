3bcdf51779eba43dbb376b9845d309d126d3e4b2
Ali <ali-alaa@gmail.com>
2025-11-06 00:47:04 +0200

    fix: correct import path for Sidebar component in ChatBot

--- src/pages/ChatBot.jsx
+++ src/pages/ChatBot.jsx
@@ -1,4 +1,4 @@
-import Sidebar from "../components/Sidebar/Sidebar";
+import Sidebar from "../components/SideBar/Sidebar";
 import Main from "../components/Main/Main";
 import { ChatBotContextProvider } from "../Context/ChatBotContext";
 
