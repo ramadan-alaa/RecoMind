4a20711d59908b01480baca5c9b0d9df8b466bcd
Ali <ali-alaa@gmail.com>
2025-12-07 00:42:24 +0200

    feat: implement verification functionality with form handling and state management

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { SignupReducer } from "./features/SignUp/SignupSlice";
 import { ForgotPasswordReducer } from "./features/ForgotPassword/ForgotPasswordSlice";
+import { VerificationReducer } from "./features/Verification/VerificationSlice";
 import { useDispatch } from "react-redux";
 
 export const store = configureStore({
   reducer: {
     signup: SignupReducer,
     forgotPassword: ForgotPasswordReducer,
+    verification: VerificationReducer,
   },
 });
 
